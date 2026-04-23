53dec2756933ae72fff84328bb2603ec40aac43e
Dzmitry Petukhou <161593819+petukhoudzmitry@users.noreply.github.com>
2026-04-23 10:45:49 +0200

    Revert changes in main branch. App again utilizes github actions instead of CloudFlare workers

--- app/src/main/java/com/tlscontact/services/URLService.kt
+++ app/src/main/java/com/tlscontact/services/URLService.kt
@@ -5,5 +5,5 @@ import javax.inject.Inject
 
 class URLService @Inject constructor() {
     var baseURL: String = "https://petukhoudzmitry.github.io/notifier"
-    var url: URL = URL("$baseURL/news.json")
+    var url: URL = URL(URL(baseURL), "news.json")
 }
